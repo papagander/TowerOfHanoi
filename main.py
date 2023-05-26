d293d7527f681ab1c37639551dd52efcd4dd2336
timdolin <tim.dolin@arcaugusta.com>
2023-05-26 19:01:19 -0400

    utilising stack class to maintain state and handle graphics

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pygame
 from sys import exit
 
-disc_count = 3
+disc_count = 5
 
 screen_height = 520
 screen_width = 1200
 
+# vertical length of discs
 disc_height = 60
 
-stack_width = 256
-stack_height = 52
+# dimensions for stack stands
+stand_width = 256
+stand_height = 52
+
+# margin between outer stacks and wall
 stack_pad = 50
-stack_x_pos = [stack_pad, (screen_width - stack_width) / 2, screen_width - stack_width - stack_pad]
 
-# vars indicating where to draw the next disc per stack
-default_empty_slot = screen_height - stack_height - disc_height
-empty_slot_y = [default_empty_slot, default_empty_slot, default_empty_slot]
-
-# vars to store which disc on top of each stack
-top_disc_size = [-1, -1, -1]
+# defines the x positions for the stack bases
+stack_x_pos = [stack_pad, (screen_width - stand_width) / 2, screen_width - stand_width - stack_pad]
+stack_y_pos = screen_height - stand_height
+# indicates y height to draw the first disc per stack
+bottom_disc_y = screen_height - stand_height - disc_height
 
 # vars used to store mouse offset when grabbing disc
 mouse_offset_x = 0
@@ -26,19 +28,17 @@ mouse_offset_y = 0
 
 # var used to store selected disc
 selected_disc = None
-
-# rects defining the area for the mouse to drop on stacks
-stack_0_drop_zone = pygame.Rect(0, 0, screen_width / 3, screen_height)
-stack_1_drop_zone = pygame.Rect(screen_width / 3, 0, screen_width / 3, screen_height)
-stack_2_drop_zone = pygame.Rect(2 * screen_width / 3, 0, screen_width / 3, screen_height)
+# stack the selected disc is from
+last_stack = None
 
 
 class Disc:
     def __init__(self, dex):
-        self.index = dex
+        self.size = dex
         self.color = get_color(dex)
-        self.width = stack_width - (1 + dex) * 32
+        self.width = stand_width - (1 + dex) * 32
     my_rect = pygame.Rect(100, 100, 100, disc_height)
+    # all discs start on the first stack
     stack_num = 0
 
 
@@ -46,65 +46,63 @@ class Stack:
     def __init__(self, dex):
         self.index = dex
         self.drop_zone = pygame.Rect(dex * screen_width / 3, 0, screen_width / 3, screen_height)
-    top_disc_size = -1
+        self.x_pos = stack_x_pos[dex]
+        self.my_discs = []
+        self.top = -1
 
-def draw_frame():
-    screen.fill(background_fill_color)
-    pygame.draw.rect(screen, 'brown', [stack_x_pos[0], screen_height - stack_height, stack_width, disc_height], width=0)
-    pygame.draw.rect(screen, 'brown', [stack_x_pos[1], screen_height - stack_height, stack_width, disc_height], width=0)
-    pygame.draw.rect(screen, 'brown', [stack_x_pos[2], screen_height - stack_height, stack_width, disc_height], width=0)
+    def push(self, new_disc):
+        if self.top != -1:
+            if new_disc.size < self.peek().size:
+                return False
+        new_disc.stack_num = self.index
+        self.top += 1
+        self.my_discs.append(new_disc)
+        self.stack_discs()
+
+    def pop(self):
+        if self.top == -1:
+            return None
+        self.top -= 1
+        popped_disc  = self.my_discs.pop()
+        self.stack_discs()
+        return popped_disc
+
+    def peek(self):
+        if self.top == -1:
+            return None
+        return self.my_discs[self.top]
+
+    def stack_discs(self):
+        disc_y = bottom_disc_y
+        for d in self.my_discs:
+            disc_x = self.x_pos + (stand_width - d.width)/2
+            d.my_rect = pygame.Rect(disc_x, disc_y, d.width, disc_height)
+            disc_y -= disc_height
+
+    def draw(self):
+        pygame.draw.rect(screen, 'brown', [self.x_pos, screen_height - stand_height, stand_width, disc_height], width=0)
+        for d in self.my_discs:
+            pygame.draw.rect(screen, d.color, d.my_rect)
+    # stores the y coord to draw the next disc pushed
+    # empty_slot_y = bottom_disc_y
 
 
-def can_add_to_stack(disc_num, stack_num):
-    if disc_num > top_disc_size[stack_num]:
+def can_add_to_stack(_disc, _stack_num):
+    top_disc = stacks[stack_num].peek()
+    if top_disc is None:
+        return True
+    if top_disc.size < _disc.size:
         return True
     return False
 
 
 def can_remove_from_stack(disc_num):
-    stack_num = discs[disc_num].stack_num
-    if disc_num == top_disc_size[stack_num]:
-        return True
-    return False
-
-
-def get_new_top_disc(stack_num):
-    top_disc_size[stack_num] = -1
-    for disc in discs:
-        if disc.stack_num == stack_num:
-            if disc.index >= top_disc_size[stack_num]:
-                top_disc_size[stack_num] = disc.index
-
-
-def get_top_slot(stack_num):
-    empty_slot_y[stack_num] = default_empty_slot
-    for disc in discs:
-        if disc.stack_num == stack_num:
-            empty_slot_y[stack_num] -= disc_height
-
-
-def add_to_stack(disc, stack_num):
-    from_stack_num = disc.stack_num
-    top_disc_size[stack_num] = disc.index
-    disc.my_rect = pygame.Rect(stack_x_pos[stack_num], empty_slot_y[stack_num],
-                               disc.width, disc_height)
-    disc.stack_num = stack_num
-    get_new_top_disc(from_stack_num)
-    top_disc_size[stack_num] = disc.index
-    empty_slot_y[from_stack_num] += disc_height
-    empty_slot_y[stack_num] -= disc_height
-    get_top_slot(from_stack_num)
-    get_top_slot(stack_num)
-
-
-def initialise(disc, stack_num):
-    from_stack_num = disc.stack_num
-    disc.stack_num = stack_num
-    top_disc_size[stack_num] = disc.index
-    disc.my_rect = pygame.Rect(stack_x_pos[stack_num], empty_slot_y[stack_num],
-                               disc.width, disc_height)
-    empty_slot_y[stack_num] -= disc_height
-    get_new_top_disc(from_stack_num)
+    _disc = all_discs[disc_num]
+    _stack = stacks[_disc.stack_num]
+    top_disc = _stack.peek()
+    if top_disc.size == _disc.size:
+        return _disc.stack_num
+    return -1
 
 
 def get_color(dex):
@@ -126,29 +124,46 @@ def get_color(dex):
 
 
 def get_mouse_zone():
-    if stack_0_drop_zone.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
-        return 0
-    if stack_1_drop_zone.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
-        return 1
-    if stack_2_drop_zone.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
-        return 2
-    else:
-        return -1
+    mouse_pos = pygame.mouse.get_pos()
+    for stack in stacks:
+        if stack.drop_zone.collidepoint(mouse_pos[0], mouse_pos[1]):
+            return stack.index
+    return -1
 
 
 def get_selection():
-    for disc in discs:
-        if disc.my_rect.collidepoint(pygame.mouse.get_pos()):
-            return disc.index
+    for d in all_discs:
+        if d.my_rect.collidepoint(pygame.mouse.get_pos()):
+            return d.size
     return -1
 
 
 def check_for_win():
-    for disc in discs:
-        if disc.stack_num != 2:
+    for d in all_discs:
+        if d.stack_num != 2:
             return False
     return True
 
+
+def draw_stacks():
+    for stack in stacks:
+        stack.draw()
+
+
+def draw_selected_disc():
+    if selected_disc is None:
+        return
+    _x = pygame.mouse.get_pos()[0] - mouse_offset_x
+    _y = pygame.mouse.get_pos()[1] - mouse_offset_y
+    pygame.draw.rect(screen, selected_disc.color, [_x, _y, selected_disc.width, disc_height], width=0)
+
+
+stacks = [Stack(0), Stack(1), Stack(2)]
+all_discs = []
+for i in range(0, disc_count):
+    all_discs.append(Disc(i))
+for disc in all_discs:
+    stacks[0].push(disc)
 
 pygame.init()
 background_fill_color = '#ccccff'
@@ -156,14 +171,8 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Tower of Hanoi')
 clock = pygame.time.Clock()
 
-test_font = pygame.font.Font('font/pixelType.ttf', 50)
-win_text = test_font.render('You Win!', False, 'darkGreen')
-
-discs = []
-for i in range(0, disc_count):
-    discs.append(Disc(i))
-for disc in discs:
-    initialise(disc, 0)
+font = pygame.font.Font('font/pixelType.ttf', 50)
+win_text = font.render('You Win!', False, 'darkGreen')
 
 running = True
 while running:
@@ -176,27 +185,24 @@ while running:
                 selection_index = get_selection()
                 if selection_index == -1:
                     continue
-                if not can_remove_from_stack(selection_index):
+                from_stack_num = can_remove_from_stack(selection_index)
+                if from_stack_num == -1:
                     continue
-                selected_disc = discs[selection_index]
-                from_stack_num = selected_disc.stack_num
-                empty_slot_y[from_stack_num] += disc_height
+                selected_disc = stacks[from_stack_num].pop()
                 mouse_offset_x = pygame.mouse.get_pos()[0] - selected_disc.my_rect[0]
                 mouse_offset_y = pygame.mouse.get_pos()[1] - selected_disc.my_rect[1]
             else:
                 stack_num = get_mouse_zone()
-                if can_add_to_stack(selected_disc.index, stack_num):
-                    add_to_stack(selected_disc, stack_num)
+                if can_add_to_stack(selected_disc, stack_num):
+                    stacks[stack_num].push(selected_disc)
                 else:
-                    add_to_stack(selected_disc, from_stack_num)
+                    stacks[from_stack_num].push(selected_disc)
                 selected_disc = None
-        if event.type == pygame.MOUSEMOTION and selected_disc is not None:
-            pos = pygame.mouse.get_pos()
-            selected_disc.my_rect = pygame.Rect(pos[0] - mouse_offset_x, pos[1] - mouse_offset_y,
-                                                selected_disc.width, disc_height)
-    draw_frame()
-    for disc in discs:
-        pygame.draw.rect(screen, disc.color, disc.my_rect)
+
+    screen.fill(background_fill_color)
+    draw_stacks()
+    draw_selected_disc()
+
     if check_for_win():
         screen.blit(win_text, (400, 20))
     pygame.display.update()
